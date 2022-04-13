b16aa075d0d4aeba45b8432f7abd4da0780a9304
JitaeWoo <94220257+JitaeWoo@users.noreply.github.com>
2022-04-13 16:49:27 +0900

    add line

--- Hello.java
+++ Hello.java
@@ -2,5 +2,6 @@ public class Hello{
   public static void main (String arg[]){
     System.out.println("hello, world");
     System.out.println("world, hello");
+    System.out.println("Wow!");
   }
 }
